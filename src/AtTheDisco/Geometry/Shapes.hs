4417076ae72d6ba0cba3379ccc1661faccfe7323
Damon Stanley <damonsava@gmail.com>
2022-01-31 12:34:38 -0500

    Add Shapes and Unify
    Replaced individual functions with a typeclass. Added a couple more
    drawable shapes.

--- src/AtTheDisco/Geometry/Shapes.hs
+++ src/AtTheDisco/Geometry/Shapes.hs
@@ -1,26 +1,54 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MonoLocalBinds        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 -- | Functions for turning shapes into layers.
-module AtTheDisco.Geometry.Shapes
-  ( boxLayer
-  , ballLayer
-  , polygonLayer
-  ) where
+module AtTheDisco.Geometry.Shapes where
 
-import           AtTheDisco.Layer       (ScreenLayer)
-import           Data.Geometry          (Arity, Point, inPolygon)
+import           AtTheDisco.Layer       (Layer, ScreenLayer)
+import           Control.Lens           ((^.))
+import           Control.Lens.TH        (makeLenses)
+import           Data.Geometry          (Arity, Line, LineSegment, Point,
+                                         Polygon, inPolygon, sqDistanceTo,
+                                         sqDistanceToSeg)
 import           Data.Geometry.Ball     (Ball, inClosedBall)
 import           Data.Geometry.Boundary (PointLocationResult (Inside, OnBoundary))
 import           Data.Geometry.Box      (Box, inBox)
+import           Data.Geometry.Matrix   (Invertible)
 import           Data.Geometry.Polygon  (Polygon, insidePolygon)
 
-boxLayer :: (Arity d, Ord r) => Box d p r -> ScreenLayer (Point d) r
-boxLayer box point = point `inBox` box
+data Thick2DLine r = Thick2DLine
+    { _thickLine     :: Line 2 r
+    , _lineThickness :: r
+    }
+    deriving (Eq, Show)
 
-ballLayer :: (Arity d, Ord r, Num r) => Ball d p r -> ScreenLayer (Point d) r
-ballLayer ball point = point `inClosedBall` ball
+$(makeLenses ''Thick2DLine)
 
-polygonLayer ::
-     (Fractional r, Ord r) => Polygon t p r -> ScreenLayer (Point 2) r
-polygonLayer poly point = (point `inPolygon` poly) `elem` [Inside, OnBoundary]
+data Thick2DLineSegment r = Thick2DLineSegment
+    { _thickLineSegment     :: LineSegment 2 () r
+    , _lineSegmentThickness :: r
+    }
+    deriving (Eq, Show)
+
+$(makeLenses ''Thick2DLineSegment)
+
+class Drawable v e where
+  draw :: v e -> ScreenLayer (Point 2) e
+
+instance Ord e => Drawable (Box 2 p) e where
+  draw box point = point `inBox` box
+
+instance (Ord e, Num e) => Drawable (Ball 2 p) e where
+  draw ball point = point `inClosedBall` ball
+
+instance (Fractional e, Ord e) => Drawable (Polygon t p) e where
+  draw poly point = (point `inPolygon` poly) `elem` [Inside, OnBoundary]
+
+instance (Fractional e, Ord e) => Drawable Thick2DLine e where
+  draw (Thick2DLine l r) point = sqDistanceTo point l <= r ^^ 2
+
+instance (Fractional e, Ord e) => Drawable Thick2DLineSegment e where
+  draw (Thick2DLineSegment s r) p = sqDistanceToSeg p s <= r ^^ 2
--- test/Test/AtTheDisco/Geometry/Shapes.hs
+++ test/Test/AtTheDisco/Geometry/Shapes.hs
@@ -2,8 +2,7 @@ module Test.AtTheDisco.Geometry.Shapes where
 
 import           Test.HUnit                        (Test, (~?))
 
-import           AtTheDisco.Geometry.Shapes        (ballLayer, boxLayer,
-                                                    polygonLayer)
+import           AtTheDisco.Geometry.Shapes        (Drawable (draw))
 import           Data.Ext                          ((:+) ((:+)))
 import           Data.Geometry                     (Point (..), Vector (..))
 import qualified Data.Geometry.Ball                as Ball
@@ -29,53 +28,53 @@ centerInBox :: Test
 centerInBox =
   let center = Point2 50 50
       smallBox = Box.fromCenter center (Vector2 40 40)
-   in boxLayer smallBox center ~? "Center in Box"
+   in draw smallBox center ~? "Center in Box"
 
 cornerInBox :: Test
 cornerInBox =
   let center = Point2 50 50
       smallBox = Box.fromCenter center (Vector2 40 40)
       corner = Point2 70 70
-   in boxLayer smallBox corner ~? "Edge in Box"
+   in draw smallBox corner ~? "Edge in Box"
 
 outsideNotInBox :: Test
 outsideNotInBox =
   let center = Point2 50 50
       smallBox = Box.fromCenter center (Vector2 40 40)
       outside = Point2 71 71
-   in not (boxLayer smallBox outside) ~? "Edge in Box"
+   in not (draw smallBox outside) ~? "Edge in Box"
 
 centerInBall =
   let center = Point2 0 0
       smallBall = Ball.fromDiameter (Point2 (-20) (-20)) (Point2 20 20)
-   in ballLayer smallBall center ~? "Center in Ball"
+   in draw smallBall center ~? "Center in Ball"
 
 edgeInBall =
   let edge = Point2 20 20
       smallBall = Ball.fromDiameter (Point2 (-20) (-20)) (Point2 20 20)
-   in ballLayer smallBall edge ~? "Edge in Ball"
+   in draw smallBall edge ~? "Edge in Ball"
 
 outsideNotInBall =
   let smallBall = Ball.fromDiameter (Point2 (-20) (-20)) (Point2 20 20)
-   in not (ballLayer smallBall (Point2 21 21)) ~? "Outside not in Ball"
+   in not (draw smallBall (Point2 21 21)) ~? "Outside not in Ball"
 
 centerInPolygon =
   let center = Point2 0 0
       rectangle =
         fromPoints . map (:+ ()) $
         [Point2 (-20) (-20), Point2 20 (-20), Point2 20 20, Point2 (-20) 20]
-   in polygonLayer rectangle center ~? "Center in Polygon"
+   in draw rectangle center ~? "Center in Polygon"
 
 cornerInPolygon =
   let edge = Point2 20 0
       rectangle =
         fromPoints . map (:+ ()) $
         [Point2 (-20) (-20), Point2 20 (-20), Point2 20 20, Point2 (-20) 20]
-   in polygonLayer rectangle edge ~? "Edge in Polygon"
+   in draw rectangle edge ~? "Edge in Polygon"
 
 outsideNotInPolygon =
   let outside = Point2 21 0
       rectangle =
         fromPoints . map (:+ ()) $
         [Point2 (-20) (-20), Point2 20 (-20), Point2 20 20, Point2 (-20) 20]
-   in not (polygonLayer rectangle outside) ~? "Outside not in Polygon"
+   in not (draw rectangle outside) ~? "Outside not in Polygon"
