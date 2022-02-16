4f11d1268672d2f1d54691cce8bc2538f7d96678
Damon Stanley <damonsava@gmail.com>
2022-02-16 18:04:22 -0500

    Fresh Start
    Think the old Pan-centric approach was not going to
    get very far. Currently, building more on top of
    hsgeometry.

--- src/AtTheDisco/Geometry/Gradient.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE DataKinds #-}
-
--- | Functions for constructing gradients
-module AtTheDisco.Geometry.Gradient where
-
-import           Data.Geometry (Affine ((.-.)), Point, Vector, dot, quadrance)
-
-lineSegmentGradient :: (Floating e) => Point 2 e -> Vector 2 e -> Point 2 e -> e
-lineSegmentGradient o v p = let u = p .-. o in (u `dot` v) / quadrance v
