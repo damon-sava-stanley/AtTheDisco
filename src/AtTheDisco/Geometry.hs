ca940d15467ce532eae66b9c778fb76d1854259b
Damon Stanley <damonsava@gmail.com>
2022-02-17 11:06:19 -0500

    Add base geometry
    Current thinking is that the system should be staged: start with an
    arrangment of 2d geometry, color and draw that, then do continuous
    transformations and finally raster transformations (i.e. traditional image
    processing)

---
+++ src/AtTheDisco/Geometry.hs
@@ -0,0 +1,351 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module AtTheDisco.Geometry
+  ( -- * Finite Geometry #fg#
+    -- $finiteGeometry
+    FiniteGeometry (..),
+
+    -- * Geometry Additions #ga#
+    -- $extensionFiniteGeometry
+    UnitInterval,
+    unitInterval,
+    viewUnitInterval,
+    Interpolable (..),
+    HasLength (..),
+    HasInside (..),
+    Projectable (..),
+
+    -- * Finite Geometries #fgs#
+    -- $finiteGeometries
+    FiniteGeometries (FiniteGeometries),
+    unwrapFiniteGeometries,
+  )
+where
+
+import Control.Lens (Identity (Identity), Lens', over, (&), (.~), (^.))
+import Control.Lens.Getter (view)
+import Control.Lens.TH (makeLenses)
+import Data.Bifunctor (Bifunctor (second))
+import Data.Ext (core, extra, type (:+) (..))
+import Data.Foldable (Foldable (toList), minimumBy)
+import Data.Function (on)
+import Data.Geometry
+  ( Dimension,
+    HasEnd (end),
+    HasIntersectionWith (intersects),
+    HasStart (start),
+    Intersection,
+    IntersectionOf,
+    IsIntersectableWith,
+    IsTransformable,
+    LineSegment,
+    MultiPolygon,
+    NoIntersection (NoIntersection),
+    NumType,
+    Point,
+    PointFunctor (pmap),
+    PolyLine,
+    Polygon,
+    SimplePolygon,
+    edgeSegments,
+    fromPointsUnsafe,
+    inPolygon,
+    interpolate,
+    listEdges,
+    outerBoundaryVector,
+    outerVertex,
+    segmentLength,
+    sqDistanceToSegArg,
+  )
+import Data.Geometry.Ball (Disk)
+import Data.Geometry.Boundary (PointLocationResult (Inside))
+import Data.Geometry.Box
+  ( IsBoxable (boundingBox),
+    Rectangle,
+    boundingBoxList,
+  )
+import qualified Data.Geometry.PolyLine as PL
+import qualified Data.Geometry.Polygon as PG
+import Data.Geometry.Properties (IsIntersectableWith (intersect))
+import Data.Geometry.Transformation (IsTransformable (transformBy))
+import Data.Intersection (IsIntersectableWith (nonEmptyIntersection))
+import Data.LSeq (LSeq, toSeq)
+import qualified Data.Sequence as Seq
+import Data.Vinyl.CoRec (CoRec (CoRec))
+import GHC.Generics (Generic)
+
+-- $finiteGeometry
+--
+-- Defines a sum type ('FiniteGeometry') for wrapping various bits of 'Data.Geometry' that fit into boxes and can be transformed.
+
+-- | 'FiniteGeometry' is geometry that can be transformed ('PointFunctor' and 'IsTransformable')
+--   and takes up finite space, i.e. 'IsBoxable'.
+data FiniteGeometry p r
+  = ATDLineSegment (LineSegment 2 p r)
+  | ATDPolyLine (PolyLine 2 p r)
+  | ATDSimplePolygon (SimplePolygon p r)
+  deriving (Generic, Eq, Show, Functor)
+
+type instance Dimension (FiniteGeometry p r) = 2
+
+type instance NumType (FiniteGeometry p r) = r
+
+instance PointFunctor (FiniteGeometry p) where
+  pmap f (ATDLineSegment v) = ATDLineSegment $ pmap f v
+  pmap f (ATDPolyLine v) = ATDPolyLine $ pmap f v
+  pmap f (ATDSimplePolygon v) = ATDSimplePolygon $ pmap f v
+
+instance (Fractional r) => IsTransformable (FiniteGeometry p r) where
+  transformBy t (ATDLineSegment v) = ATDLineSegment $ transformBy t v
+  transformBy t (ATDPolyLine v) = ATDPolyLine $ transformBy t v
+  transformBy t (ATDSimplePolygon v) = ATDSimplePolygon $ transformBy t v
+
+instance IsBoxable (FiniteGeometry p r) where
+  boundingBox (ATDLineSegment v) = boundingBox v
+  boundingBox (ATDPolyLine v) = boundingBox v
+  boundingBox (ATDSimplePolygon v) = boundingBox v
+
+type instance IntersectionOf (Point 2 r) (PolyLine 2 p r) = '[NoIntersection, Point 2 r]
+
+type instance IntersectionOf (Point 2 r) (FiniteGeometry p r) = '[NoIntersection, Point 2 r]
+
+instance (Num r, Ord r) => HasIntersectionWith (Point 2 r) (PolyLine 2 p r) where
+  intersects p pl = any (\ls -> p `intersects` ls) (edgeSegments pl)
+
+instance (Fractional r, Ord r) => HasIntersectionWith (Point 2 r) (FiniteGeometry p r) where
+  intersects p (ATDLineSegment v) = intersects p v
+  intersects p (ATDPolyLine v) = intersects p v
+  intersects p (ATDSimplePolygon v) = intersects p v
+
+-- $extensionFiniteGeometry
+--
+-- Classes which extend the functionality of "Data.Geometry"
+
+-- | A 'UnitInterval' carries a value in the inclusive range [0, 1].
+newtype UnitInterval a = UnitInterval a deriving (Generic, Eq, Ord, Show)
+
+unitInterval :: (Ord a, Num a) => a -> UnitInterval a
+unitInterval x
+  | x < 0 = UnitInterval 0
+  | x > 1 = UnitInterval 1
+  | otherwise = UnitInterval x
+
+viewUnitInterval :: UnitInterval a -> a
+viewUnitInterval (UnitInterval a) = a
+
+polygonToPolyLine :: Polygon t p r -> PolyLine 2 p r
+polygonToPolyLine = PL.fromPointsUnsafe . PG.toPoints
+
+-- | An 'Interpolable' shape is one whose boundary can be indexed by a number on the unit interval,
+-- and which, further, can be divided at a certain point. These should satisfy the following laws.
+--
+-- Assuming, as should be true, that the result of 'splitInterp' is itself 'Interpolable'
+--
+-- prop> interp x s = interp 1 (fst (splitInterp x s)
+-- prop> interp x s = interp 0 (snd (splitInterp x s)
+-- prop> interp 0 s = interp 0 (fst (splitInterp x s)
+-- prop> interp 1 s = interp 1 (snd (splitInterp x s))
+--
+-- More generally. For \(x < y\),
+--
+-- prop> interp x s = interp (x/y) (fst (splitInterp y) s)
+-- prop> interp y s = interp ((y-x)/(1-x)) (snd (splitInterp x) s)
+--
+-- Assuming that the shape 'HasLength'.
+--
+-- prop> shapeLength (fst (splitInterp x s)) = viewUnitInterval x * shapeLength s
+-- prop> shapeLength (snd (splitInterp x s)) = (1 - viewUnitInterval x) * shapeLength s
+--
+-- Assuming that the shape 'IsIntersectableWith' 'Point' 2 r, then
+--
+-- prop> intersects (interp x s) s
+--
+-- Finally, the function 'interpAndSplit' is offered as a potentially more convenient to implement
+-- bundle, which may also reduce wasted computation in case both interpolation and splitting are needed.
+--
+-- Minimum implementation: either 'interpAndSplit' or 'interp' and 'splitInterp'
+class Interpolable f r where
+  -- | The result of splitting a shape into two parts. Should itself be 'Interpolable'; may be the
+  --   original type.
+  type InterpolationSplit f :: * -> *
+
+  -- | Performs both 'interp' and 'splitInterp' at a given interpolation percent.
+  interpAndSplit :: UnitInterval r -> f r -> (Point 2 r, (InterpolationSplit f r, InterpolationSplit f r))
+  interpAndSplit u r = (interp u r, splitInterp u r)
+
+  -- | Find a point on the shape that is a given percent along the boundary.
+  interp :: UnitInterval r -> f r -> Point 2 r
+  interp u r = fst (interpAndSplit u r)
+
+  -- | Divide a shape into two sub-shapes, one of which contains the boundary.
+  -- Warning: values of 0 and 1 may produce pathological results.
+  splitInterp :: UnitInterval r -> f r -> (InterpolationSplit f r, InterpolationSplit f r)
+  splitInterp u r = snd (interpAndSplit u r)
+
+instance (Fractional r) => Interpolable (LineSegment 2 p) r where
+  type InterpolationSplit (LineSegment 2 p) = LineSegment 2 p
+  interp (UnitInterval x) ls = interpolate x ls
+  splitInterp (UnitInterval x) ls = (left, right)
+    where
+      p = interpolate x ls
+      -- TODO: This doesn't properly set the Open/Closed state. Don't know that it will matter
+      left = ls & end . core .~ p
+      right = ls & start . core .~ p
+
+instance forall r p. (Ord r, Floating r) => Interpolable (PolyLine 2 p) r where
+  type InterpolationSplit (PolyLine 2 p) = PolyLine 2 p
+
+  interpAndSplit u pl = go u pl
+    where
+      go :: UnitInterval r -> PolyLine 2 p r -> (Point 2 r, (PolyLine 2 p r, PolyLine 2 p r))
+      go u pl =
+        let (p, sl, sr) = go' (viewUnitInterval u * shapeLength pl) [] (toList . edgeSegments $ pl)
+         in (p, (fromPointsUnsafe (points sl), fromPointsUnsafe (points sr)))
+
+      points :: [LineSegment 2 p r] -> [Point 2 r :+ p]
+      points [] = []
+      points (h : xs) = h ^. start : map (^. end) (h : xs)
+
+      go' :: r -> [LineSegment 2 p r] -> [LineSegment 2 p r] -> (Point 2 r, [LineSegment 2 p r], [LineSegment 2 p r])
+      go' target past (current : future) =
+        let l = shapeLength current
+         in if target <= l
+              then
+                let (p, (sl, sr)) = interpAndSplit (unitInterval (target / l)) current
+                 in (p, reverse (sl : past), sr : future)
+              else go' (target - l) (current : past) future
+      go' _ _ [] = error "Should not run off the edge in Polyline interpAndSplit"
+
+instance (Ord r, Floating r) => Interpolable (SimplePolygon p) r where
+  type InterpolationSplit (SimplePolygon p) = PolyLine 2 p
+
+  interpAndSplit u pg = interpAndSplit u (polygonToPolyLine pg)
+
+instance (Ord r, Floating r) => Interpolable (FiniteGeometry p) r where
+  type InterpolationSplit (FiniteGeometry p) = FiniteGeometry p
+
+  interpAndSplit x (ATDLineSegment ls) = let (p, (lsS, lsE)) = interpAndSplit x ls in (p, (ATDLineSegment lsS, ATDLineSegment lsE))
+  interpAndSplit x (ATDPolyLine ls) = let (p, (lsS, lsE)) = interpAndSplit x ls in (p, (ATDPolyLine lsS, ATDPolyLine lsE))
+  interpAndSplit x (ATDSimplePolygon ls) = let (p, (lsS, lsE)) = interpAndSplit x ls in (p, (ATDPolyLine lsS, ATDPolyLine lsE))
+
+-- | A shape 'HasLength' when it has a length. Always nonnegative.
+class HasLength f r where
+  -- | The length of the shape.
+  shapeLength :: f r -> r
+
+instance (Floating r) => HasLength (LineSegment 2 p) r where
+  shapeLength = segmentLength
+
+instance (Floating r) => HasLength (PolyLine 2 p) r where
+  shapeLength = foldr (\ls acc -> acc + shapeLength ls) 0 . edgeSegments
+
+instance (Floating r) => HasLength (SimplePolygon p) r where
+  shapeLength = shapeLength . polygonToPolyLine
+
+instance (Floating r) => HasLength (FiniteGeometry p) r where
+  shapeLength (ATDLineSegment ls) = shapeLength ls
+  shapeLength (ATDPolyLine pl) = shapeLength pl
+  shapeLength (ATDSimplePolygon pg) = shapeLength pg
+
+-- | Shapes which have an inside. Note that 1D shape have trivial insides, i.e. nothing is inside them.
+class HasInside f p r where
+  getInside :: Point 2 r -> f p r -> Maybe p
+
+instance HasInside (LineSegment 2) p r where
+  getInside p v = Nothing
+
+instance HasInside (PolyLine 2) p r where
+  getInside p v = Nothing
+
+instance (Fractional r, Ord r) => HasInside SimplePolygon p r where
+  getInside p v = if p `inPolygon` v == Inside then Just payload else Nothing
+    where
+      payload = v ^. outerVertex 0 . extra
+
+instance (Fractional r, Ord r) => HasInside FiniteGeometry p r where
+  getInside p (ATDLineSegment v) = getInside p v
+  getInside p (ATDPolyLine v) = getInside p v
+  getInside p (ATDSimplePolygon v) = getInside p v
+
+-- | To "project" a point on to a shape is to find the point on the boundary of the shape closest to the given point.
+class Projectable f p r where
+  -- | Find the point on the shape closest to the given point.
+  project ::
+    -- | the point to query
+    Point 2 r ->
+    -- | the shape
+    f p r ->
+    -- | a pair of the distance squared to the projected point and the projected point
+    (r, Point 2 r :+ p)
+
+instance (Fractional r, Ord r) => Projectable (LineSegment 2) p r where
+  project p seg = second (:+ payload) $ sqDistanceToSegArg p seg
+    where
+      -- choice of payload is arbitrarily that of the start.
+      payload = seg ^. start . extra
+
+-- Making this a `Projectable` instance would require some type level composition I don't feel like wrestling with.
+projectNonEmptyFoldableLineSegments ::
+  (Foldable t, Ord a, Functor t, Projectable f p a) =>
+  Point 2 a ->
+  t (f p a) ->
+  (a, Point 2 a :+ p)
+projectNonEmptyFoldableLineSegments p =
+  minimumBy (compare `on` fst) . fmap (project p)
+
+instance (Fractional r, Ord r) => Projectable (PolyLine 2) p r where
+  project p = projectNonEmptyFoldableLineSegments p . toSeq . edgeSegments
+
+instance (Fractional r, Ord r) => Projectable SimplePolygon p r where
+  project p poly = projectNonEmptyFoldableLineSegments p . listEdges $ poly
+
+instance (Fractional r, Ord r) => Projectable FiniteGeometry p r where
+  project p (ATDLineSegment l) = project p l
+  project p (ATDPolyLine l) = project p l
+  project p (ATDSimplePolygon l) = project p l
+
+-- $finiteGeometries
+--
+-- It is handy to deal with collection of shapes, which is what 'FiniteGeometries' provides. It has much of the same functionality,
+-- but because it is not a single shape.
+
+-- | `FiniteGeometries` represents a non-empty collection of `FiniteGeometry`. When this matters, objects earlier
+--   in the sequence are considered on top.
+newtype FiniteGeometries p r = FiniteGeometries
+  { _unwrapFiniteGeometries :: LSeq 1 (FiniteGeometry p r) }
+  deriving (Eq, Show, Generic, Functor)
+
+$(makeLenses ''FiniteGeometries)
+
+type instance Dimension (FiniteGeometries p r) = 2
+
+type instance NumType (FiniteGeometries p r) = r
+
+instance PointFunctor (FiniteGeometries p) where
+  pmap f = over unwrapFiniteGeometries (fmap (pmap f))
+
+instance IsBoxable (FiniteGeometries p r) where
+  boundingBox = boundingBoxList . view unwrapFiniteGeometries
+
+instance (Fractional r, Ord r) => Projectable FiniteGeometries p r where
+  project p = projectNonEmptyFoldableLineSegments p . view unwrapFiniteGeometries
+
+instance (Fractional r, Ord r) => HasInside FiniteGeometries p r where
+  getInside r = foldr (\x acc -> getInside r x `firstJust` acc) Nothing . view unwrapFiniteGeometries
+    where
+      firstJust (Just a) _ = Just a
+      firstJust _ b = b
+
+-- No `Monoid` as we are requiring nonempty.
+instance Semigroup (FiniteGeometries p r) where
+  (FiniteGeometries l1) <> (FiniteGeometries l2) = FiniteGeometries (l1 <> l2)
+
+-- Helper (unexported) functions
---
+++ test/Test/AtTheDisco/GeometrySpec.hs
@@ -0,0 +1,294 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Test.AtTheDisco.GeometrySpec (spec) where
+
+import AtTheDisco.Geometry
+import Control.Arrow (Arrow (second))
+import Control.Lens (toListOf, view, (^.), (^..))
+import Control.Monad (liftM2)
+import Data.Ext (core, extra, type (:+))
+import Data.Foldable (Foldable (toList))
+import Data.Geometry
+  ( HasEnd (end),
+    HasStart (start),
+    IsTransformable (transformBy),
+    LineSegment (LineSegment'),
+    Point (Point2),
+    PointFunctor (pmap),
+    PolyLine,
+    SimplePolygon,
+    Transformation (Transformation),
+    Vector,
+    qdA,
+  )
+import Data.Geometry.Box (IsBoxable (boundingBox))
+import Data.Geometry.Matrix (Matrix (Matrix))
+import qualified Data.Geometry.PolyLine as PL
+import qualified Data.Geometry.Polygon as PG
+import Data.Intersection (HasIntersectionWith (intersects))
+import Data.List (nubBy)
+import Data.Maybe (catMaybes, fromJust, mapMaybe)
+import Data.Vector.Circular (CircularVector)
+import qualified Data.Vector.Circular as CV
+import GHC.Generics (Generic)
+import System.Random (Random (..))
+import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
+
+-- Arbitrary Geometreies
+
+points :: [LineSegment 2 p r] -> [Point 2 r :+ p]
+points (h : xs) = h ^. start : fmap (^. end) (h : xs)
+points _ = []
+
+instance forall p r. (Eq r, Floating r, Arbitrary p, Arbitrary r) => Arbitrary (PolyLine 2 p r) where
+  arbitrary = fmap (PL.fromPointsUnsafe . points) c
+    where
+      c :: Gen [LineSegment 2 p r]
+      c =
+        sized
+          ( \n -> do
+              s <- choose (2, n + 2)
+              toList . PL.edgeSegments . PL.fromPointsUnsafe <$> vector s
+          )
+          `suchThat` all (\s -> shapeLength s /= 0)
+  shrink = mapMaybe PL.fromPoints . shrink . toList . view PL.points
+
+instance Arbitrary1 CircularVector where
+  liftArbitrary gen = sized $ \n -> do
+    l <- choose (1, n)
+    CV.replicate1M l gen
+  liftShrink s v = mapMaybe (CV.fromList . s) . CV.toList $ v
+
+instance forall p r. (Arbitrary p, Arbitrary r, Eq r, Num r) => Arbitrary (SimplePolygon p r) where
+  arbitrary = fmap PG.fromCircularVector cv3Uniq
+    where
+      cv3 :: Gen (CircularVector (Point 2 r :+ p))
+      cv3 = CV.cons <$> arbitrary <*> (CV.cons <$> arbitrary <*> arbitrary)
+      cv3Uniq :: Gen (CircularVector (Point 2 r :+ p))
+      cv3Uniq =
+        cv3
+          `suchThat` ( isJust
+                         . foldr
+                           ( \a m -> do
+                               b <- m
+                               if b /= a ^. core then Just (a ^. core) else Nothing
+                           )
+                           (Just (Point2 0 0))
+                     )
+      isJust (Just _) = True
+      isJust Nothing = False
+  shrink = fmap PG.fromCircularVector . shrink . fromJust . CV.fromVector' . PG.toVector
+
+instance forall p r. (Arbitrary p, Arbitrary r, Eq r, Floating r) => Arbitrary (FiniteGeometry p r) where
+  arbitrary = do
+    t <- chooseInt (0, 2)
+    case t of
+      0 -> fmap ATDLineSegment (arbitrary :: Gen (LineSegment 2 p r))
+      1 -> fmap ATDPolyLine (arbitrary :: Gen (PolyLine 2 p r))
+      _ -> fmap ATDSimplePolygon (arbitrary :: Gen (SimplePolygon p r))
+  shrink (ATDLineSegment v) = fmap ATDLineSegment . shrink $ v
+  shrink (ATDPolyLine v) = fmap ATDPolyLine . shrink $ v
+  shrink (ATDSimplePolygon v) = fmap ATDSimplePolygon . shrink $ v
+
+instance forall p r. (Arbitrary p, Arbitrary r, Eq r, Floating r) => Arbitrary (FiniteGeometries p r) where
+  arbitrary = fmap FiniteGeometries arbitrary
+  shrink (FiniteGeometries seq) = fmap FiniteGeometries . shrink $ seq
+
+instance Arbitrary1 (Transformation 2) where
+  liftArbitrary (gen :: Gen a) = Transformation . Matrix <$> genVec3Vec3
+    where
+      genVec3 :: Gen (Vector 3 a)
+      genVec3 = liftArbitrary gen
+
+      genVec3Vec3 :: Gen (Vector 3 (Vector 3 a))
+      genVec3Vec3 = liftArbitrary genVec3
+
+instance (Arbitrary a) => Arbitrary (Transformation 2 a) where
+  arbitrary = liftArbitrary arbitrary
+
+instance (Arbitrary a, Random a, Ord a, Num a) => Arbitrary (UnitInterval a) where
+  arbitrary = unitInterval <$> choose (0, 1)
+
+-- Convenience synonyms
+
+type LS2F = LineSegment 2 Int FloatingNaNWrapper
+
+type PL2F = PolyLine 2 Int FloatingNaNWrapper
+
+type PGF = SimplePolygon Int FloatingNaNWrapper
+
+type T2F = Transformation 2 FloatingNaNWrapper
+
+type UIUR = UnitInterval UR
+
+type LS2UR = LineSegment 2 Int UR
+
+type PL2UR = PolyLine 2 Int UR
+
+type PG2UR = SimplePolygon Int UR
+
+type FGUR = FiniteGeometry Int UR
+
+-- Wrappers
+
+-- A wrapper around `Float` to change the behavior so that NaN == NaN
+newtype FloatingNaNWrapper = FloatingNaNWrapper Float deriving (Arbitrary, Num, Fractional, Floating, Ord)
+
+instance Eq FloatingNaNWrapper where
+  (FloatingNaNWrapper a) == (FloatingNaNWrapper b) = (isNaN a && isNaN b) || (a == b)
+
+instance Show FloatingNaNWrapper where
+  show (FloatingNaNWrapper a) = show a
+
+newtype FloatInUnitInterval = FloatInUnitInterval Float deriving (Eq, Num, Fractional, Ord)
+
+instance Arbitrary FloatInUnitInterval where
+  arbitrary = FloatInUnitInterval <$> choose (0, 1)
+
+instance Show FloatInUnitInterval where
+  show (FloatInUnitInterval x) = show x
+
+-- A Rational drawn arbitrarily on the unit interval
+newtype UR = UR Rational deriving (Eq, Num, Ord, Fractional, Show)
+
+asDouble :: (Double -> Double) -> UR -> UR
+asDouble f (UR x) = UR . toRational . f . fromRational $ x
+
+instance Floating UR where
+  pi = UR . toRational $ (pi :: Double)
+  exp = asDouble exp
+  log = asDouble log
+  sin = asDouble sin
+  cos = asDouble cos
+  asin = asDouble asin
+  acos = asDouble acos
+  atan = asDouble atan
+  sinh = asDouble sinh
+  cosh = asDouble cosh
+  asinh = asDouble sinh
+  acosh = asDouble acosh
+  atanh = asDouble atanh
+
+instance Random UR where
+  randomR (UR l, UR u) g = let (b, g') = randomR (fromRational l :: Double, fromRational u) g in (UR (toRational b), g')
+
+  random g = randomR (0, 1) g
+
+instance Arbitrary UR where
+  arbitrary = UR <$> arbitrarySizedFractional
+
+overUnits :: (Ord a, Fractional a) => (a -> a -> a) -> UnitInterval a -> UnitInterval a -> UnitInterval a
+overUnits g x y = unitInterval (viewUnitInterval x `g` viewUnitInterval y)
+
+divUnit :: (Ord a, Fractional a) => UnitInterval a -> UnitInterval a -> UnitInterval a
+divUnit = overUnits (/)
+
+subUnit :: (Ord a, Fractional a) => UnitInterval a -> UnitInterval a -> UnitInterval a
+subUnit = overUnits (-)
+
+newtype CloseEnough a = CloseEnough a deriving (Show)
+
+-- Obviously this violates every law known to man, but it's just for tests.
+instance Eq (CloseEnough UR) where
+  (CloseEnough x) == (CloseEnough y) = abs (x - y) < 1e-8
+
+instance Eq (CloseEnough (Point 2 UR)) where
+  (CloseEnough p1) == (CloseEnough p2) = qdA p1 p2 < 1e-8
+
+-- Test
+
+pass :: Expectation
+pass = 1 `shouldBe` 1
+
+guard :: Bool -> Expectation -> Expectation
+guard True e = pass
+guard False e = e
+
+shouldBeCloseEnough :: (Show a, Eq (CloseEnough a)) => a -> a -> Expectation
+shouldBeCloseEnough x y = shouldBe (CloseEnough x) (CloseEnough y)
+
+spec :: Spec
+spec = do
+  -- Basic typeclass tests
+  describe "FiniteGeometry passes through boxes" $ do
+    prop "with LineSegment" $
+      \(x :: LS2F) -> boundingBox x `shouldBe` boundingBox (ATDLineSegment x)
+    prop "with PolyLine" $
+      \(x :: PL2F) -> boundingBox x `shouldBe` boundingBox (ATDPolyLine x)
+    prop "with SimplePolygon" $
+      \(x :: PGF) -> boundingBox x `shouldBe` boundingBox (ATDSimplePolygon x)
+  describe "FiniteGeometry passes through transformations" $ do
+    prop "with LineSegment" $
+      \(x :: LS2F) (t :: T2F) -> (ATDLineSegment . transformBy t) x `shouldBe` transformBy t (ATDLineSegment x)
+    prop "with PolyLine" $
+      \(x :: PL2F) (t :: T2F) -> (ATDPolyLine . transformBy t) x `shouldBe` transformBy t (ATDPolyLine x)
+    prop "with Polygon" $
+      \(x :: PGF) (t :: T2F) -> (ATDSimplePolygon . transformBy t) x `shouldBe` transformBy t (ATDSimplePolygon x)
+  describe "FiniteGeometry passes through pmap" $ do
+    prop "with LineSegment" $
+      \(x :: LS2F) (t :: T2F) -> (ATDLineSegment . pmap (transformBy t)) x `shouldBe` pmap (transformBy t) (ATDLineSegment x)
+    prop "with PolyLine" $
+      \(x :: PL2F) (t :: T2F) -> (ATDPolyLine . pmap (transformBy t)) x `shouldBe` pmap (transformBy t) (ATDPolyLine x)
+    prop "with Polygon" $
+      \(x :: PGF) (t :: T2F) -> (ATDSimplePolygon . pmap (transformBy t)) x `shouldBe` pmap (transformBy t) (ATDSimplePolygon x)
+  describe "UnitInterval" $ do
+    prop "clamps between 0 and 1" $
+      \(x :: Float) -> (viewUnitInterval . unitInterval) x `shouldSatisfy` (\y -> y <= 1 && y >= 0)
+    prop "otherwise does nothing" $
+      \(x :: FloatInUnitInterval) -> (viewUnitInterval . unitInterval) x `shouldBe` x
+
+  describe "Polygon interpolation" $ do
+    prop "interpolants on line segment" $
+      \(x :: UIUR) (ls :: FGUR) -> interp x ls `shouldSatisfy` (`intersects` ls)
+    prop "interpolant ends first half" $
+      \(x :: UIUR) (ls :: FGUR) ->
+        interp (unitInterval 1) (fst (splitInterp x ls))
+          `shouldBe` interp x ls
+    prop "interpolant begins secnd  half" $
+      \(x :: UIUR) (ls :: FGUR) ->
+        interp (unitInterval 0) (snd (splitInterp x ls))
+          `shouldBe` interp x ls
+    prop "start of first half is start of whole" $
+      \(x :: UIUR) (ls :: FGUR) ->
+        interp (unitInterval 0) (fst (splitInterp x ls))
+          `shouldBe` interp (unitInterval 0) ls
+    prop "end of second half is end of whole" $
+      \(x :: UIUR) (ls :: FGUR) ->
+        interp (unitInterval 1) (snd (splitInterp x ls))
+          `shouldBe` interp (unitInterval 1) ls
+    prop "picking subportion of first works" $
+      \(x :: UIUR) (y :: UIUR) (ls :: FGUR) ->
+        let x' = min x y; y' = max x y
+         in guard (y' == unitInterval 0) $
+              interp x' ls
+                `shouldBeCloseEnough` interp (divUnit x' y') (fst (splitInterp y' ls))
+    prop "picking subportion of second works" $
+      \(x :: UIUR) (y :: UIUR) (ls :: FGUR) ->
+        let x' = min x y; y' = max x y
+         in if x' == unitInterval 1
+              then pass
+              else
+                interp y' ls
+                  `shouldBeCloseEnough` interp (divUnit (subUnit y' x') (subUnit (unitInterval 1) x')) (snd (splitInterp x' ls))
+    prop "first half has proportional length" $
+      \(x :: UIUR) (ls :: FGUR) ->
+        shapeLength (fst $ splitInterp x ls) `shouldBeCloseEnough` (viewUnitInterval x * shapeLength ls)
+    prop "second half has proportional length" $
+      \(x :: UIUR) (ls :: FGUR) ->
+        shapeLength (snd $ splitInterp x ls) `shouldBeCloseEnough` ((1 - viewUnitInterval x) * shapeLength ls)
+  describe "Projection Tests" $
+    prop "interpolated points are their own projection" $
+      (\(x :: UIUR) (ls :: FGUR) ->
+        let p = interp x ls
+         in second (^. core) (project p ls)
+              `shouldBe` (0, p))
+    prop "projected points intersect shape" $ 
+      \(p :: Point 2 UR) (ls :: FGUR) ->
+        let (_, p') = project p ls in p' `shouldSatisfy` (`intersects` ls)
