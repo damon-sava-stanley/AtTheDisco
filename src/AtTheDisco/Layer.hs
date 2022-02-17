296c2d08a93a0a34dcc56f8dd598ce3201f96d2d
Damon Stanley <damonsava@gmail.com>
2022-02-17 15:36:07 -0500

    Add basic drawing capabilities
    Allow us to trace and fill our shapes.

--- src/AtTheDisco/Layer.hs
+++ src/AtTheDisco/Layer.hs
@@ -1,28 +1,79 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module AtTheDisco.Layer(
-    -- * Types
-    Layer, 
-    ScreenLayer, 
-    ColorLayer,
-    -- * util
-    unwrapVector,
-    mask
-    ) where
+module AtTheDisco.Layer
+  ( -- * Drawing Geometry
+    -- $drawingGeometry
+    GeometryStyle (GeometryStyle),
+    geometryThickness,
+    geometryLineColor,
+    geometryFill,
+    Drawable (..),
+  )
+where
 
-import Graphics.Color.Model (Color)
-import Data.Geometry (Point(..), Arity)
+import AtTheDisco.Geometry
+  ( FiniteGeometries,
+    HasInside (getInside),
+    Projectable (project),
+  )
+import Control.Lens (over, (^.))
+import Control.Lens.TH (makeLenses)
+import Data.Ext (type (:+) ((:+)))
+import Data.Geometry (Dimension, NumType, Point)
+import Data.Geometry.Box (IsBoxable (boundingBox))
+import Data.Maybe (fromMaybe)
+import Data.Sequence (Seq)
 
--- | A layer is a named thing.
-type Layer v a b = v a -> b
+-- $drawingGeometry
+--
+-- Given an assembly of shapes, see "AtTheDisco.Geometry", provide functions for coloring those
+-- shapes and producing a Pan-style continuous image.
 
-type ScreenLayer v a = Layer v a Bool
+-- | Style information for coloring a piece of geometry.
+data GeometryStyle r c = GeometryStyle
+  { -- | The radius of the lines.
+    _geometryThickness :: r,
+    -- | The color of the line. `Nothing` if invisble (should be `Nothing` if the diameter is 0).
+    _geometryLineColor :: Maybe c,
+    -- | How this should be filled. Can be an arbitrary pattern.
+    --   Two layers of maybe: the fill as a whole could be `Nothing`, but if it is
+    --   not the colors should help.
+    _geometryFill :: Maybe (Point 2 r -> Maybe c)
+  }
+  deriving (Functor)
 
-type ColorLayer v a m e = Layer v a (Color m e)
+$(makeLenses ''GeometryStyle)
 
-unwrapVector :: Layer (Point 2) a b -> a -> a -> b
-unwrapVector f x y = f (Point2 x y)
+instance (Show r, Show c) => Show (GeometryStyle r c) where
+  show (GeometryStyle t lc f) =
+    "GeometryStyle " <> show t <> " " <> show lc <> " "
+      <> (case f of Just _ -> "Just _"; Nothing -> "Nothing")
 
-mask :: ScreenLayer v a -> Layer v a b -> Layer v a b -> Layer v a b
-mask test onTrue onFalse coord = if test coord then onTrue coord else onFalse coord
+-- | A `Drawable` shape can be samples for color at any point.
+class Drawable f r c where
+  draw :: f r -> Point 2 r -> c
+
+instance
+  (Ord r, Num r, HasInside f (GeometryStyle r c) r, Projectable f (GeometryStyle r c) r) =>
+  Drawable (f (GeometryStyle r c)) r (Maybe c)
+  where
+  draw shape point = lineColor `firstJust` insideColor
+    where
+      (sqDist, p :+ cfg) = project point shape
+      r = cfg ^. geometryThickness
+      lineColor = if sqDist <= r * r then cfg ^. geometryLineColor else Nothing
+      inside = getInside point shape
+      insideColor = do
+        cfg' <- inside
+        let fill = cfg' ^. geometryFill
+        fill' <- fill
+        fill' point
+      firstJust (Just x) _ = Just x
+      firstJust _ y = y
---
+++ test/Test/AtTheDisco/LayerSpec.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Test.AtTheDisco.LayerSpec where
+
+import AtTheDisco.Geometry
+import AtTheDisco.Layer
+import Control.Lens
+import Data.Bifunctor (Bifunctor (first))
+import Data.Geometry
+import Test.AtTheDisco.ArbitraryGeometry
+import Test.AtTheDisco.GeometryTypes
+import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
+import qualified Data.LSeq as LSeq 
+import Data.Data ( Proxy(..) )
+
+instance Arbitrary2 GeometryStyle where
+  liftArbitrary2 genR genC = do
+    thickness <- genR
+    color <- liftArbitrary genC
+    fillColor <- liftArbitrary genC
+    let fill = fmap (\c x -> Just c) fillColor
+    return $ GeometryStyle thickness color fill
+
+instance Arbitrary r => Arbitrary1 (GeometryStyle r) where
+  liftArbitrary = liftArbitrary2 arbitrary
+
+instance (Arbitrary r, Arbitrary c) => Arbitrary (GeometryStyle r c) where
+  arbitrary = liftArbitrary arbitrary
+
+spec :: Spec
+spec = do
+  describe "Drawing Geometry" $ do
+    prop "points on boundary drawn" $ do
+      \(c :: Int) (s :: GeometryStyle UR Int) (x :: UIUR) (sh :: FGUR) ->
+        draw (first (const $s & geometryLineColor ?~ c) sh) (interp x sh)
+          `shouldBe` Just c
+    prop "inside of a polygon drawn" $ do
+      \(c :: Int) (PolygonAroundPoint p pg :: PolygonAroundPoint Int UR) ->
+        let pg' = first (const $ GeometryStyle (0 :: UR) Nothing (Just . const $ Just c)) pg
+         in draw pg' p `shouldBe` Just c
+    prop "first polygon wins" $ do
+      \(c :: Int) (PolygonAroundPoint p pg :: PolygonAroundPoint Int UR) ->
+        let pg' = first (const $ GeometryStyle (0 :: UR) Nothing (Just . const $ Just c)) pg
+            pg'' = first (const $ GeometryStyle (0 :: UR) Nothing (Just . const $ Just (c + 1))) pg
+            fgs = FiniteGeometries (LSeq.forceLSeq (Proxy :: Proxy 1) . LSeq.fromList $
+                    [ATDSimplePolygon pg', ATDSimplePolygon pg''])
+         in draw fgs p `shouldBe` Just c
